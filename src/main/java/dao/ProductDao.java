a906e29bb9896557a243c5c47f2cc3f6deee42ba
jaekwon Lee <ljg10sky@naver.com>
2023-06-09 14:02:23 +0900

    Feat ProductDao/ 상품이름으로 상품조회하는 기능 추가

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import util.DaoHelper;
 import vo.Product;
+import vo.ProductCategory;
 
 public class ProductDao {
 	
@@ -17,5 +18,25 @@ public class ProductDao {
 													 product.getStock(),
 													 product.getDescription(),
 													 product.getProductCategory().getNo());
+	}
+	
+	public Product getProductByName(String name) {
+		return DaoHelper.selectOne("", rs -> {
+			Product product = new Product();
+			product.setNo(rs.getInt("product_no"));
+			product.setName(rs.getString("product_name"));
+			product.setPrice(rs.getInt("product_price"));
+			product.setStock(rs.getInt("product_stock"));
+			product.setCreateDate(rs.getDate("product_create_date"));
+			product.setUpdateDate(rs.getDate("product_update_date"));
+			product.setSoldOut(rs.getString("product_sold_out"));
+			product.setDescription(rs.getString("product_description"));
+			
+			ProductCategory cat = new ProductCategory();
+			cat.setNo(rs.getInt("cat_no"));
+			product.setProductCategory(cat);
+			
+			return product;
+		}, name);
 	}
 }
