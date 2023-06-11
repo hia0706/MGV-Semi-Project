363335f88dabb7c1ceb7aad36dedffa74f74ff92
jaekwon Lee <ljg10sky@naver.com>
2023-06-11 10:14:29 +0900

    Feat ProductDao에서 번호로 상품조회기능 추가 및  상품 정보수정 기능 추가

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -13,6 +13,16 @@ public class ProductDao {
 	public static ProductDao getInstance() {
 		return instance;
 	}
+	
+	public void updateProduct(Product product) {
+		DaoHelper.update("productDao.updateProduct", product.getName(),
+							 						 product.getPrice(),
+							 						 product.getStock(),
+							 						 product.getSoldOut(),
+							 						 product.getDescription(),
+							 						 product.getProductCategory().getNo(),
+							 						 product.getNo());
+	}
 
 	public void insertProduct(Product product) {
 		DaoHelper.update("productDao.insertProduct", product.getName(),
@@ -42,9 +52,31 @@ public class ProductDao {
 		}, name);
 	}
 	
+	public Product getProductByNo(int no) {
+		return DaoHelper.selectOne("productDao.getProductByNo", rs -> {
+			Product product = new Product();
+			product.setNo(rs.getInt("product_no"));
+			product.setName(rs.getString("product_name"));
+			product.setPrice(rs.getInt("product_price"));
+			product.setStock(rs.getInt("product_stock"));
+			product.setCreateDate(rs.getDate("product_create_date"));
+			product.setUpdateDate(rs.getDate("product_update_date"));
+			product.setSoldOut(rs.getString("product_sold_out"));
+			product.setDescription(rs.getString("product_description"));
+			
+			ProductCategory cat = new ProductCategory();
+			cat.setNo(rs.getInt("cat_no"));
+			cat.setName(rs.getString("cat_name"));
+			product.setProductCategory(cat);
+			
+			return product;
+		}, no);
+	}
+	
 	public List<Product> getProducts(int begin, int end) {
 		return DaoHelper.selectList("productDao.getProducts", rs -> {
 			Product product = new Product();
+			product.setNo(rs.getInt("product_no"));
 			product.setName(rs.getString("product_name"));
 			product.setPrice(rs.getInt("product_price"));
 			product.setStock(rs.getInt("product_stock"));
